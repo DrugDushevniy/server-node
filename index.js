7706c98a580113d261516e8f91044108dbef0f05
strupsts <strupsts3@mail.ru>
2023-02-25 16:34:50 +0500

    AUTH LOGICK SERVER

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:3000"
+    origin: "http://87.249.44.57:3000"
 }));
 app.use("/auth", authRouter);
 app.use(errorMiddleware);
