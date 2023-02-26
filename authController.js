f52e9c550cbbd01916f63a4f5e62bb0835a90df6
strupsts <strupsts3@mail.ru>
2023-02-26 13:27:03 +0500

    test

--- authController.js
+++ authController.js
@@ -46,11 +46,10 @@ class authController {
     async refresh (req,res,next) {
         try{
             const {refreshToken} = req.cookies;
-            console.log(`КУККИ Auth Controller ((${req.cookies}))`)
-            console.log(`refreshToken Auth Controller ((${refreshToken}))`)
+            console.log(`КУККИ Auth Controller (())`,req.cookies)
+            console.log(`refreshToken Auth Controller (())`, refreshToken)
             const userData = await userService.refresh(refreshToken)
-            console.log(`USERDATA(НОВЫЕ ТОКЕНЫ) Auth Controller ((${userData}))`)
-            console.log
+            console.log(`USERDATA(НОВЫЕ ТОКЕНЫ) Auth Controller (())`, userData)
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30*24*60*60*1000, httpOnly: true})
             return res.json(userData)
         }
