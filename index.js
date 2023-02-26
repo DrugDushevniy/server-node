5965ec1cca94c8ac8d79402d3465c52284b7007b
strupsts <strupsts3@mail.ru>
2023-02-26 18:05:13 +0500

    test

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = 5000;
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:3000"
+    origin: ["http://1253919-ca24285.tw1.ru", "http://localhost:3000", "http://87.249.44.57"]
 
 }))
 app.use(express.json())
